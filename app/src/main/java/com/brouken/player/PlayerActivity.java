ad23214ed70f3f02344741d9f868cf068ac236b4
nonelocker <126131646+nonelocker@users.noreply.github.com>
2025-08-19 11:19:06 +0900

    Update PlayerActivity.java

--- app/src/main/java/com/brouken/player/PlayerActivity.java
+++ app/src/main/java/com/brouken/player/PlayerActivity.java
@@ -771,21 +771,21 @@ public class PlayerActivity extends Activity {
             intent.putExtra(API_END_BY, playbackFinished ? "playback_completion" : "user");
             if (!playbackFinished) {
                 if (player != null) {
-                    long duration = player.getContentDuration()-player.getCurrentPosition();
-                    //if (duration != C.TIME_UNSET) {
-                    //    intent.putExtra(API_DURATION, (int) player.getDuration());
-                    //}
-                    //else
-                    //{
-                        intent.putExtra(API_DURATION, (int)duration );
-                    //}
+                    long duration = player.getDuration();
+                    if (duration != C.TIME_UNSET) {
+                        intent.putExtra(API_DURATION, (int) player.getDuration());
+                    }
+                    else
+                    {
+                        intent.putExtra(API_DURATION, (int)2147483647 );
+                    }
                     if (player.isCurrentMediaItemSeekable()) {
-                        //if (mPrefs.persistentMode) {
-                            //intent.putExtra(API_POSITION, (int) mPrefs.nonPersitentPosition);
-                        //} else {
+                        if (mPrefs.persistentMode) {
+                            intent.putExtra(API_POSITION, (int) mPrefs.nonPersitentPosition);
+                        } else {
                             intent.putExtra(API_POSITION, (int) player.getCurrentPosition());
                             //intent.putExtra(API_DURATION, ((int) player.getDuration() - (int) player.getCurrentPosition()));
-                        //}
+                        }
                     }
                 }
             }
